7e64abec2833c32a2c367d54ea81578f279ff1a5
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2026-07-24 18:14:22 +0200

    docs: add license headers to source and test files
    Datadog OSS release policy requires each source file to carry a
    license notice pointing back to the repository license.

--- src/ddgl/format/_parser.py
+++ src/ddgl/format/_parser.py
@@ -1,3 +1,6 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/) Copyright 2026 Datadog, Inc.
+
 """Parser for GitLab CI job traces.
 
 Single source of truth for all trace-related regexes. Builds a ``Trace``
