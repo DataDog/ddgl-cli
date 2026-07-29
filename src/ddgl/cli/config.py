7ba2c3fc6f81a999302b7c4261ae8cfc6d227e2f
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2026-07-29 16:13:21 +0200

    feat(cli): Add `ddgl config show` subcommand

--- src/ddgl/cli/config.py
+++ src/ddgl/cli/config.py
@@ -5,11 +5,18 @@
 
 from __future__ import annotations
 
+import asyncio
+from typing import TYPE_CHECKING
+
+import msgspec
 import rich_click as click
 
-from ddgl.config import get_config_file_path
+from ddgl.config import get_config_file_path, load_config, load_config_file
 from ddgl.render._console import console
 
+if TYPE_CHECKING:
+    from ddgl.config import Config
+    from ddgl.model import ConfigFile
 
 @click.group()
 def config_group() -> None:
@@ -32,4 +39,55 @@ def path(
 
     console.print(path)
 
+
+@click.command()
+@click.option(
+    "--json", "output_json", is_flag=True, default=False, help="Output as JSON."
+)
+@click.option(
+    "--raw",
+    "-r",
+    "output_raw",
+    is_flag=True,
+    default=False,
+    help="Show the raw loaded config file instead of the fully-resolved config",
+)
+@click.option(
+    "--censor/--no-censor",
+    "censor",
+    is_flag=True,
+    default=True,
+    help="Hide sensitive data from output (tokens...)",
+)
+def show(output_json: bool, output_raw: bool, censor: bool) -> None:
+    """Shows the currently resolved Config object"""
+    data: ConfigFile | Config
+
+    if output_raw:
+        data = load_config_file()
+    else:
+        data = asyncio.run(load_config())
+        if censor:
+            data = msgspec.structs.replace(data, private_token="*" * 10)
+
+    if output_json:
+        # Don't use print_json, we don't really care about pretty-printing
+        console.print(msgspec.json.encode(data).decode())
+        return
+
+    # Sanitize any Nones before exporting as toml
+    sanitized = _strip_none(msgspec.to_builtins(data))
+    console.print(msgspec.toml.encode(sanitized).decode())
+
+def _strip_none(obj):
+    if obj is None:
+        return "N/A"
+    if isinstance(obj, dict):
+        return {k: _strip_none(v) for k, v in obj.items()}
+    if isinstance(obj, list):
+        return [_strip_none(v) for v in obj]
+    return obj
+
+
 config_group.add_command(path)
+config_group.add_command(show)
